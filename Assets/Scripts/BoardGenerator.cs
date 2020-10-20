873c9ae40e9c62e346e57026567206128b0723c7
Ayush Kumar <ayushrocks456@gmail.com>
2020-10-20 13:07:02 +0530

    Generated A board

---
+++ Assets/Scripts/BoardGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGenerator: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject TilePrimitive;
+    public GameObject Board;
+    [SerializeField]
+    private Vector2 firstTilePosition = Vector2.zero;
+    [SerializeField]
+    private float xOffset;
+    [SerializeField]
+    private float xOffsetIncrease = 0;
+    [SerializeField]
+    private float yOffset;
+    [SerializeField]
+    private float yOffsetIncrease = 0;
+
+    private float spawnXPos;
+    private float spawnYPos;
+
+
+
+
+    void Start()
+    {
+        SetupBoard(8, 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetupBoard(int width , int height)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            yOffset += yOffsetIncrease;
+            spawnYPos = firstTilePosition.y - yOffset;
+            xOffset = 0;
+            for (int j = 0; j < width; j++)
+            {
+                xOffset += xOffsetIncrease;
+                spawnXPos = firstTilePosition.x + xOffset;
+                Instantiate(TilePrimitive, new Vector3(spawnXPos, spawnYPos,
+                    0.0f), Quaternion.identity, Board.transform);
+               
+            }
+            
+        }
+    }
+}
